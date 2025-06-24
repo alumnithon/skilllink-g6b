7ded6699029206b7672563aaf614dc9b5276ec35
emanuel peracchia <emanuelperacchia1@gmail.com>
2025-06-23 21:46:07 -0300

    feat(perfil): implementar gestión de perfil de usuario

---
+++ src/main/java/alumnithon/skilllink/domain/userprofile/dto/CountryPrivateDto.java
@@ -0,0 +1,13 @@
+package alumnithon.skilllink.domain.userprofile.dto;
+
+public class CountryPrivateDto {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
